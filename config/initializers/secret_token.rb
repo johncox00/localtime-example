526c99d5a8da8ef86f0899a8f84e4077b03153a4
johncox00 <john@mach19.com>
2013-11-13 10:13:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocaltimeTest::Application.config.secret_key_base = '5b302f047b1c710496b5984523796a7d15c2383e2fd203e5b3c1f56efa2a2a3eb56f6b756d696d8f74769f301ff90f69f33dcd923cf24522e07354f587806ab6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocaltimeTest::Application.config.session_store :cookie_store, key: '_localtime-test_session'
